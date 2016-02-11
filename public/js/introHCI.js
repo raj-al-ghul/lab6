0c20dba97ae8b6c554633edf9a2b0286d196b647
Dilraj Singh <dilrajsingh.7@gmail.com>
2016-02-11 13:14:51 -0800

    lab4 - stretch goal errors
    errytime

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,18 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get('/project/'+idNumber, addProject);
+}
+
+function addProject(result) {
+	console.log(result);
+
+	$('#project'+result.id +' .details').html(
+		'<img src=' + result.image + ' class="detailsImage">'
+		+result.title+ ' (' +result.date + ')'
+		+result.summary 
+	);
 }
 
 /*
@@ -35,4 +47,77 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get('/palette', changeColors);
+	console.log("here")
+	// $.ajax({
+	// 	type: 'GET',
+	// 	url: 'http://api.worldbank.org/countries/all/indicators/SP.POP.TOTL?format=json',
+	// 	// async: false,
+	// 	// jsonpCallback: 'jsonCallback',
+	// 	contentType: "application/json",
+	// 	crossDomain: true,
+	// 	dataType: 'jsonp',
+
+	// 	success: function(result) {
+	// 		console.log('test');
+	// 		console.log(result);
+	// 	},
+	// 	error: function(e) {
+	// 	   console.log(e);
+	// 	}
+	// });
+	// var xhr = new XMLHttpRequest();
+	// xhr.open('GET', 'http://data.nba.com/json/cms/2015/standings/conference.json', true);
+	// xhr.send(null);
+	// xhr.onreadystatechange = function () {
+	//   var DONE = 4; // readyState 4 means the request is done.
+	//   var OK = 200; // status 200 is a successful return.
+	//   if(xhr.readyState === DONE) {
+	//   	if(xhr.status === OK) {
+	// 		console.log(xhr.responseText); // 'This is the returned text.'
+	//   	} else {
+	//   		console.log('Error: ' + xhr.status); // An error occurred during the request.
+	//   	}
+	//   }
+	// };
+	$.get('http://pokeapi.co/api/v2/pokemon/1/', lololol, 'jsonp');
+}
+
+function changeColors(result) {
+	//console.log(result);
+	var colors = result.randomPalette.colors.hex;
+
+	//console.log(colors)
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+
+
+	//$.get('http://api.worldbank.org/countries/all/indicators/SP.POP.TOTL?format=json', 'jsonp', lololol);
+
+	// var url = 'http://api.worldbank.org/countries/all/indicators/SP.POP.TOTL?format=json';
+
+	// $.ajax({
+	// 	type: 'GET',
+	// 	url: 'http://api.worldbank.org/countries/all/indicators/SP.POP.TOTL?format=json',
+	// 	async: false,
+	// 	jsonpCallback: 'jsonCallback',
+	// 	contentType: "application/json",
+	// 	dataType: 'jsonp',
+	// 	success: function(result) {
+	// 		console.log('test');
+	// 		console.log(result);
+	// 	},
+	// 	error: function(e) {
+	// 	   console.log(e);
+	// 	}
+	// });
+
+}
+
+function lololol(result) {
+	console.log(result);
 }
